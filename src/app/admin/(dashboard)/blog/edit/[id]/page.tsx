70e75d98b2041f8ce6181ba8a9c1f2b6b5f5071d
AjitKumarRoy <ak47riderji@gmail.com>
2025-09-20 03:14:03 +0530

    TypeScript Error

--- src/app/admin/(dashboard)/blog/edit/[id]/page.tsx
+++ src/app/admin/(dashboard)/blog/edit/[id]/page.tsx
@@ -1,4 +1,4 @@
-import type { PageProps } from 'next';
+// import type { PageProps } from 'next';
 import { BlogForm } from "../../components/BlogForm";
 import { BlogPost } from "@/lib/models";
 import dbConnect from "@/lib/dbConnect";
@@ -9,8 +9,8 @@ async function getPost(id: string) {
   return JSON.parse(JSON.stringify(post));
 }
 
-export default async function EditBlogPostPage({ params }: PageProps<{ id: string }>) {
-  const { id } = await params;        
+export default async function EditBlogPostPage({ params }: {  params: { id: string }}) {
+  const { id } =  params;        
   const post = await getPost(id);
  
   return <BlogForm post={post} />;
